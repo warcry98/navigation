e747aa3f79a7981d8858f31bec7de613f553af1c
warcry98 <72479786+warcry98@users.noreply.github.com>
2022-12-06 10:42:52 +0700

    Initial commit

---
+++ lib/Screens/login_screen.dart
@@ -0,0 +1,133 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  LoginScreenState createState() => LoginScreenState();
+}
+
+class LoginScreenState extends State<LoginScreen> {
+  final userIdController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          margin: EdgeInsets.only(
+            top: 50,
+            left: 20,
+            right: 20,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                margin: EdgeInsets.only(
+                  bottom: 12,
+                ),
+                child: Text(
+                  'Login',
+                  style: TextStyle(
+                    fontSize: 30,
+                  ),
+                ),
+              ),
+              textInput(
+                'User Id',
+                'enter userid',
+                userIdController,
+              ),
+              textInput(
+                'Password',
+                'enter password',
+                passwordController,
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: ElevatedButton(
+                  onPressed: () {
+                    doLogin();
+                  },
+                  child: Text('Login'),
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.only(
+                      left: 40,
+                      right: 40,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget textInput(
+    inputName,
+    hintText,
+    controller,
+  ) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(inputName),
+        Container(
+          alignment: Alignment.center,
+          height: 40,
+          margin: EdgeInsets.only(
+            top: 10,
+            bottom: 20,
+          ),
+          child: TextFormField(
+            controller: controller,
+            textAlignVertical: TextAlignVertical.center,
+            decoration: InputDecoration(
+                hintText: hintText,
+                contentPadding: EdgeInsets.only(
+                  left: 12,
+                  top: 0,
+                  bottom: 0,
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                )),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Future<void> doLogin() async {
+    var formData = FormData.fromMap({
+      "userid": userIdController.text,
+      "psw": passwordController.text,
+      "op": "login",
+    });
+
+    var response = await Dio().post(
+      'http://nusantarapowerrembang.com/flutter/log1.php',
+      data: formData,
+    );
+
+    // print(response.data);
+    Map<String, dynamic> data =
+        jsonDecode(response.data) as Map<String, dynamic>;
+
+    String level = data['level'];
+
+    // if (level == "User") {
+    //   Navigator.pushNamed(context, '/LoggedUser');
+    // } else {
+    //   ScaffoldMessenger.of(context)
+    //       .showSnackBar(SnackBar(content: Text('Login Failed')));
+    // }
+  }
+}
